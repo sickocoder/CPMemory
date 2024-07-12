036074e5e3a1103af32c4eafa2ac956c6c1b152d
José Tomé <jose.tome@standardbank.co.ao>
2024-07-12 14:40:42 +0100

    Initial Commit

---
+++ CPMemory/CPMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPMemoryApp.swift
+//  CPMemory
+//
+//  Created by A255085 on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CPMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPMemory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CPMemory
+//
+//  Created by A255085 on 12/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
